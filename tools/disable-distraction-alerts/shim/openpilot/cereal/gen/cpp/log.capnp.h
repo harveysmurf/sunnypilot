6d664c52a1e13785abd0bb267b0cbac10704099f
wildboar <wildboar@nanobot>
2026-08-27 13:34:23 +0300

    fix: build comma params library against persistent store

--- tools/disable-distraction-alerts/shim/openpilot/cereal/gen/cpp/log.capnp.h
+++ tools/disable-distraction-alerts/shim/openpilot/cereal/gen/cpp/log.capnp.h
@@ -3,6 +3,15 @@
 namespace cereal {
 enum class LongitudinalPersonality : uint16_t { AGGRESSIVE = 0, STANDARD = 1, RELAXED = 2 };
 struct InitData {
-  enum class DeviceType : uint16_t { UNKNOWN = 0, PC = 1, TICI = 2 };
+  enum class DeviceType : uint16_t {
+    UNKNOWN = 0,
+    NEO = 1,
+    CHFFR_ANDROID = 2,
+    CHFFR_IOS = 3,
+    TICI = 4,
+    PC = 5,
+    TIZI = 6,
+    MICI = 7,
+  };
 };
 }
